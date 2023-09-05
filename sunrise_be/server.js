2dd2ece46b54a78f0f0b46e0b6b5273f24ceadaa
Aswandcp <aswand@sayonetech.com>
2023-09-05 10:58:56 +0530

    total calculation

--- sunrise_be/server.js
+++ sunrise_be/server.js
@@ -439,7 +439,7 @@ app.post("/create-payment-intent", async (req, res) => {
   const currency = req.body.currency;
   const order = await commerceTools.createOrder(cart, "Open");
 
-  let total = cart.totalPrice.centAmount;
+  let subtotal = cart.totalPrice.centAmount;
   let summary = "";
   cart.lineItems.forEach((item) => {
     const price = item.price.value.centAmount;
@@ -447,7 +447,7 @@ app.post("/create-payment-intent", async (req, res) => {
   });
 
   const payload = {
-    amount: total,
+    amount: subtotal,
     currency: currency,
     metadata: {
       summary: summary,
@@ -473,7 +473,7 @@ app.post("/create-payment-intent", async (req, res) => {
   const payment = await commerceTools.createPayment(
     paymentIntent,
     currency,
-    total
+    subtotal
   );
   commerceTools.updatePaymentState("Authorization", paymentIntent);
 
